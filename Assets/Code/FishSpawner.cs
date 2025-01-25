203da01c622027776a0642bcc3179f2222fd0a9f
hedvig <hedvig.reutersward@gmail.com>
2025-01-25 14:07:32 +0100

    fish spawner has maximum variable of fish spawns at once

--- Assets/Code/FishSpawner.cs
+++ Assets/Code/FishSpawner.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FishSpawner : MonoBehaviour
 {
@@ -11,46 +13,53 @@ public class FishSpawner : MonoBehaviour
     [SerializeField] private int spawnRadius = 10;
     
     [Header("Spawn Time Settings")]
-    [SerializeField] private int minTimeBetweenSpawns = 15;
+    [SerializeField] private int minTimeBetweenSpawns = 5;
     [SerializeField] private int maxTimeBetweenSpawns = 60;
 
     [Header("Lifetime Settings")]
     [SerializeField] private int minLifetime = 10;
     [SerializeField] private int maxLifetime = 90;
 
+    private int maximumFishParticlesAtOnce = 5;
+    private int currentNumberOfFish = 0;
     private List<GameObject> objectPool = new();
     private float elapsedTime = 0f;
-
-
-private void Start()
+    
+    private void Start()
     {
         StartCoroutine(SpawnRoutine());
     }
-
+    
     private IEnumerator SpawnRoutine()
     {
         float randomLifetime = Random.Range(minLifetime, maxLifetime);
-        float randomSpawnDelay = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
         while (elapsedTime < randomLifetime)
         {
-            // Increment elapsed time by the time between spawns
-            // float spawnDelay = Random.Range(minSpawnTime, maxSpawnTime + 1);
             elapsedTime += Time.deltaTime;
-            yield return new WaitForSeconds(randomSpawnDelay);
-
-            if (elapsedTime <= randomLifetime)
+            if (currentNumberOfFish < maximumFishParticlesAtOnce)
             {
-                GameObject obj = GetPooledObject();
-                obj.transform.position = GetRandomPositionAroundPlayer();
-                obj.SetActive(true);
-                // float lifetime = Random.Range(minLifetime, maxLifetime + 1);
-                // StartCoroutine(DeactivateAfterTime(obj, lifetime));
+                float randomSpawnDelay = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+                yield return new WaitForSeconds(randomSpawnDelay);
+
+                if (elapsedTime <= randomLifetime)
+                {
+                    GameObject obj = GetPooledObject();
+                    obj.transform.position = GetRandomPositionAroundPlayer();
+                    obj.SetActive(true);
+                    currentNumberOfFish++;
+                    float randomFishLifetime = Random.Range(minLifetime, maxLifetime);
+                    StartCoroutine(DeactivateObjectAfterTime(obj, randomFishLifetime));
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(1f);
             }
         }
         gameObject.SetActive(false);
     }
 
-    private IEnumerator DeactivateAfterTime(GameObject obj, float time)
+    private IEnumerator DeactivateObjectAfterTime(GameObject obj, float time)
     {
         yield return new WaitForSeconds(time);
         obj.SetActive(false);
@@ -65,7 +74,6 @@ private void Start()
                 return pooledObj;
             }
         }
-
         GameObject newObj = Instantiate(fishPrefab);
         objectPool.Add(newObj);
         return newObj;
