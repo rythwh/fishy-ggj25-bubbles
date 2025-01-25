b3060317a77a93b8d327b32ef8102575032d6812
Ryan White <6309098+rythwh@users.noreply.github.com>
2025-01-25 14:49:17 +0100

    Update Player.cs

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,3 +1,5 @@
+using System;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,16 +7,24 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 movementInput;
-    
-    public void OnMove(InputAction.CallbackContext context)
-    {
+
+    public event Action<Vector2> OnPlayerMoved;
+
+    [UsedImplicitly]
+    public void OnMove(InputAction.CallbackContext context) {
         movementInput = context.ReadValue<Vector2>();
-        Debug.Log("Moving!");
     }
 
-    private void Update()
-    {
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
         Vector3 moveDirection = new Vector3(movementInput.x, 0f, movementInput.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+        if (Mathf.Approximately(moveDirection.magnitude, 0)) {
+            return;
+        }
+        OnPlayerMoved?.Invoke(transform.position);
     }
 }
