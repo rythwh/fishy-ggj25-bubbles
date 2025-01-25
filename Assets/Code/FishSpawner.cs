a00e782cb1ae87b2e87c332a682147fa0a8046fd
hedvig <hedvig.reutersward@gmail.com>
2025-01-25 12:02:47 +0100

    Adding a fishParticles prefab, creating fishspawner script

---
+++ Assets/Code/FishSpawner.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private GameObject fishPrefab;
+
+    private void SpawnFish(int radius)
+    {
+        
+    }
+}
